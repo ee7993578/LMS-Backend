af9a0293362de4c97bd1a75752b48dc57dbcb8df
ee7993578 <ee7993578@gmail.com>
2026-07-04 13:21:39 +0530

    d

--- src/main/java/com/learningJWT/LearningTemplate/LearningTemplateApplication.java
+++ src/main/java/com/learningJWT/LearningTemplate/LearningTemplateApplication.java
@@ -1,5 +1,5 @@
 package com.learningJWT.LearningTemplate;
-
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class LearningTemplateApplication {
 
 	public static void main(String [] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(LearningTemplateApplication.class, args);
 	}
 
